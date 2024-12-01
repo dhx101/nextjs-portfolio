7fca4c8663e7cde8b31089327b9077e7e4f025e9
David Huang Xie <138037766+dhx101@users.noreply.github.com>
2024-12-01 22:50:18 +0100

    created dist and getting ready to deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
+	// output: "export", // <=== enables static exports
+	distDir: "dist",
+	reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    }
 };
 
 export default nextConfig;
