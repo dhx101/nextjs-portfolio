c8c049e5faae8d89b515568f23dcc056da282d34
David Huang Xie <138037766+dhx101@users.noreply.github.com>
2024-12-04 14:20:11 +0100

    basepath to my page

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	output: "export", // <=== enables static exports
-	// basePath: "/nextjs-portfolio",
+	basePath: "/nextjs-portfolio",
 	distDir: "dist",
 	reactStrictMode: true,
 	images: {
