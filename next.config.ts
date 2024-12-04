28ca4bef65dbf0d88ca3385ce09e0e982f62ed14
David Huang Xie <138037766+dhx101@users.noreply.github.com>
2024-12-04 21:39:27 +0100

    final portfolio

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	output: "export", // <=== enables static exports
-	distDir: "build",
+	distDir: "dist",
 	reactStrictMode: true,
 	images: {
 		unoptimized: true,
